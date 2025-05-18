df25b1302d321841c972cc2a92c6fa3f47588453
Elaiza-art <liza.kupidonli25@mail.ru>
2025-05-18 19:55:34 +0500

    юхууу

--- Assets/Scripts/AngryBirrd.cs
+++ Assets/Scripts/AngryBirrd.cs
@@ -4,6 +4,8 @@ public class AngryBirrd : MonoBehaviour
 {
     [SerializeField]
     private float speed = 9.0f;
+    [SerializeField]
+    private int damage = -1;
     private Vector3 moveVector;
     private float randomSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -13,6 +15,13 @@ public class AngryBirrd : MonoBehaviour
         moveVector = new Vector3(-randomSpeed, 0);
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+        Destroy(gameObject);
+        collision.gameObject.GetComponent<MoveBird>().updateLife(damage);
+        collision.gameObject.GetComponent<ParticleSystem>().Play();
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/MoveBird.cs
+++ Assets/Scripts/MoveBird.cs
@@ -1,19 +1,27 @@
+using TMPro;
 using UnityEngine;
 
 public class MoveBird : MonoBehaviour
 {
     [SerializeField]
+    TextMeshProUGUI lifeStatus;
+    [SerializeField]
     private float speed = 5.0f;
+    [SerializeField]
+    int lifeCounter = 3;
     private Vector3 moveVector;
     public int moveDirection = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveVector = new Vector3(0, speed);
-
+        lifeStatus.text = lifeCounter.ToString();
     }
-
-    // Update is called once per frame
+    public void updateLife(int life)
+    {
+        lifeCounter += life;
+        lifeStatus.text = lifeCounter.ToString();
+    }// Update is called once per frame
     void Update()
     {
         if (transform.position.y < 3.71 && moveDirection == 1)
