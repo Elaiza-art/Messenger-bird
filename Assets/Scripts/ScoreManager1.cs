cfe3c4266e4a01189557be696489777751da8583
Elaiza-art <liza.kupidonli25@mail.ru>
2025-06-14 18:29:12 +0500

    Исрпавила

--- Assets/Scripts/ScoreManager1.cs
+++ Assets/Scripts/ScoreManager1.cs
@@ -1,20 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 
 public class ScoreManager1 : MonoBehaviour
 {
     public int scoreCounter = 0;
     [SerializeField]
     private TextMeshProUGUI score;
+    [SerializeField]
+    public int winScore = 25;
     void Start()
     {
-        score.text = "—чет: " + scoreCounter;
+        score.text = "—чет: " + scoreCounter + " / 25";
     }
 
     public void addToScore()
-    { 
+    {
         scoreCounter++;
-        score.text = "—чет: " + scoreCounter;
+        score.text = "—чет: " + scoreCounter + " / 25";
+        if (scoreCounter >= winScore)
+        {
+            LoadNextLevel();
+        }
+    }
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene("Cutscene_Level1");
     }
 }
